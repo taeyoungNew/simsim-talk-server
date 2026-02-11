0647adfe6f4796391fedeb0d7a7147eb590657e8
taeyoungNew <dndbxhd10@naver.com>
2026-02-11 16:28:19 +0900

    socket cors version 2

--- src/sockets/socket.server.ts
+++ src/sockets/socket.server.ts
@@ -2,24 +2,25 @@ import { Server } from "socket.io";
 let io: Server;
 
 export const initSocketServer = (httpServer: any) => {
+  console.log("🔥 SOCKET CORS VERSION 2");
   io = new Server(httpServer, {
     cors: {
-      origin: [
-        "http://localhost:5173",
-        "https://simsim-talk-client.vercel.app",
-      ],
+      // origin: [
+      //   "http://localhost:5173",
+      //   "https://simsim-talk-client.vercel.app",
+      // ],
       // origin: `${process.env.CORS_ORIGIN}`,
-      // origin: (origin, callback) => {
-      //   if (
-      //     !origin ||
-      //     origin.endsWith(".vercel.app") ||
-      //     origin === "http://localhost:5173"
-      //   ) {
-      //     callback(null, true);
-      //   } else {
-      //     callback(new Error("Not allowed by CORS"));
-      //   }
-      // },
+      origin: (origin, callback) => {
+        if (
+          !origin ||
+          origin.endsWith(".vercel.app") ||
+          origin === "http://localhost:5173"
+        ) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
       methods: ["GET", "POST"],
       credentials: true,
     },
