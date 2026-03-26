54bf0bb1ac445c3a549f567f5690f35959bbab72
taeyoungNew <dndbxhd10@naver.com>
2026-03-26 20:54:38 +0900

    isLogoutMiddleware

--- src/routes/chatRouter.ts
+++ src/routes/chatRouter.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
 import ChatHandler from "../handlers/chatHandler";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { isLogoutMiddleware } from "../middlewares/isLogout.middleware";
 
 const chatRouter = Router();
 const chatHandler = new ChatHandler();
 
 // 채팅방만들기
-chatRouter.post("/create-chatroom", authMiddleware, chatHandler.createChatRoom);
+chatRouter.post(
+  "/create-chatroom",
+  isLogoutMiddleware,
+  authMiddleware,
+  chatHandler.createChatRoom,
+);
 // 나의 채팅방리스트 가져오기
-chatRouter.get("/get-chatlist", authMiddleware, chatHandler.getChatList);
+chatRouter.get(
+  "/get-chatlist",
+  isLogoutMiddleware,
+  authMiddleware,
+  chatHandler.getChatList,
+);
 
 export default chatRouter;
