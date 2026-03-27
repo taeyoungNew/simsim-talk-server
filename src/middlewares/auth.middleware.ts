ec32f7ab84373b6f7bc0d8980f84053567c34c65
taeyoungNew <dndbxhd10@naver.com>
2026-03-27 23:48:31 +0900

    domain cors

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -144,7 +144,7 @@ export const authMiddleware = async (
         res.cookie("authorization", `Bearer ${newAccToken}`, {
           httpOnly: true,
           secure: true,
-          sameSite: "strict",
+          sameSite: "lax",
           maxAge: 1000 * 60 * 30,
           expires: new Date(Date.now() + 1000 * 60 * 30),
         });
